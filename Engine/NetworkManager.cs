553cdd5b62d513f2c017a2f8ae07c9ad213ef968
BigMac <Aaronbmwhitehead@gmail.com>
2018-04-14 21:56:22 -0700

    Outlined method of network manager getting data from other managers

---
+++ Engine/NetworkManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+    class NetworkManager : IEngineComponent
+    {
+        //this is going to host websockets/rest api endpoint.
+
+        /*
+         *
+         * Here is how this is going to work:
+         * Entity manager is going to have an function that handles updating entity list.
+         * When that is called, it will throw an event which networkmanager is going to be subscribed to. It will pass a reference to its entity list.
+         *
+         * World manager will do the same thing, in that when you edit the list of items in the world, it will throw an event, telling the network manager to update its internal list.
+         *
+         * These will be non blocking updates against the owin(?) websocket/rest api stuff.
+         *
+         *
+         */
+        public void Tick()
+        {
+            
+        }
+    }
+}
